8714acbe3e6641d3a86b11ec69ee4923384b7d5e
Chris Scanlon <chris@scanlon.id.au>
2020-06-10 12:21:33 +1000

    Initial commit

---
+++ app/CustomerImport.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerImport extends Model
+{
+    //
+}
---
+++ app/Http/Controllers/CustomerImportController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CustomerImport;
+use Illuminate\Http\Request;
+
+class CustomerImportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CustomerImport  $customerImport
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CustomerImport $customerImport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CustomerImport  $customerImport
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CustomerImport $customerImport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CustomerImport  $customerImport
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CustomerImport $customerImport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CustomerImport  $customerImport
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CustomerImport $customerImport)
+    {
+        //
+    }
+}
